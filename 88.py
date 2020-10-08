a8dd260e442f6507291b57a6e44d3eb2ffe2d6c6
Kate Wu <70921725+katewu1994@users.noreply.github.com>
2020-10-08 23:18:07 +0900

    Merge Sorted Array

--- 88.py
+++ 88.py
@@ -8,3 +8,11 @@ class Solution:
         for i in nums2:
             nums1.append(i)
         nums1.sort()
+        
+        
+"""
+.pop(): 指定した位置の要素を削除し、値を取得
+.append()  末尾に要素を追加
+.sort()   昇順にソート
+.sort(reverse=True)   降順にソート
+"""
